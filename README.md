f4f8eaf95d91cc1ed86f3a4eaa9b1250f20fa457
saikksub <saikksub@gmail.com>
2019-03-07 09:18:37 +0530

    First commit with working code

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# nativescript-vue-demo-tabview-custom
-Nativescript Vue demo implementation of custom TabView with icons
+# NativeScript Vue custom TabView Demo with Icons
+
+Nativescript Vue demo implementation of custom TabView with icons.
+
+> This repository make use of nativescript-vue's `FlexboxLayout` and `GridLayout` to design basic TabView with font awesome icons.
+
+<p align="center">
+<img
+  src="screenshot/demo.png"
+  width="auto" height="400">
+</p>
+
+## Install
+``` bash
+git clone https://github.com/saikksub/nativescript-vue-demo-tabview-custom.git
+cd nativescript-vue-demo-tabview-custom
+cd demo
+npm install
+```
+
+## Demo
+``` bash
+tns run <platform> --bundle
+```
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
+
+## Author
+[saikksub](https://github.com/saikksub)
---
+++ demo/app/components/App.vue
@@ -0,0 +1,74 @@
+<template>
+    <Page
+        actionBarHidden="false">
+        <ActionBar
+            title="Custom TabView Demo"
+            flat>
+        </ActionBar>
+        <GridLayout
+            rows="*,60">
+            <StackLayout row="0" orientation="vertical">
+                <GridLayout columns="*" rows="*">
+                    <Label :text="msg" col="0" row="0" style="text-align: center;"/>
+                </GridLayout>
+            </StackLayout>
+            <FlexboxLayout row="1" alignItems="center" justifyContent="space-between" class="bottom-navigation">
+                <StackLayout
+                    v-for="(item, index) in nav"
+                    :key="index"
+                    class="hero-view"
+                    width="80"
+                    orientation="vertical"
+                    @tap="onTapTab(item)">
+                    <Label
+                        :text="`${(item.icon )}` | fonticon"
+                        class="fa icon" />
+                    <Label
+                        :text="item.title"
+                        class="nav-label" />
+                </StackLayout>
+            </FlexboxLayout>
+        </GridLayout>
+    </Page>
+
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Home',
+        nav: [
+          { title: 'Home', icon: 'fa-home' },
+          { title: 'List', icon: 'fa-list' },
+          { title: 'Cards', icon: 'fa-id-card' },
+          { title: 'Settings', icon: 'fa-cog' }
+        ]
+      }
+    },
+    methods: {
+      onTapTab: function ({ title }) {
+        this.msg = title
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+    .hero-view {
+      vertical-align: center;
+      text-align: center;
+      font-size: 25;
+      color: #333333;
+    }
+    .nav-label {
+      font-size: 14;
+    }
+    .bottom-navigation {
+      background: rgb(225, 229, 236);
+    }
+</style>
